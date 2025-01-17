7bc1a9094307fe7e6bcbd91fe3d7793689b1eb66
Mike Thomas <mikethomas84@gmail.com>
2025-01-17 12:45:17 +0000

    run through test init

---
+++ tests/Feature/RunThroughTest.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Set up a product comfig
+pull and order from shopify - turn it into orderlines
+generate WFs from those orderlines (match product)
+generate artworks from those wfs
+(mark those artworks as complete)
+check they appear in the queue - mark them as ready
+check the wfs are marked as ready
+mark as sent to supplier
+generate a supplier update (check URL is active)
+check that that update is process and WFs are updated
+*/
+
+test('product setup', function () {
+
+});
