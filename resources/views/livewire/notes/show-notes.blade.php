7291ca5af53d2da3841f21f9429ae0bd1590d1e3
Mike Thomas <mikethomas84@gmail.com>
2025-01-21 16:14:10 +0000

    basic with

--- resources/views/livewire/notes/show-notes.blade.php
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -3,9 +3,14 @@
 use Livewire\Volt\Component;
 
 new class extends Component {
-    //
+    public function with(): array
+    {
+        return [
+            'title' => "Show Notes",
+        ];
+    }
 }; ?>
 
 <div>
-    //
+    {{ $title }}
 </div>
