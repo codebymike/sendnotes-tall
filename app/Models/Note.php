c0c62df3ea7871862c284eed5725944053001296
Mike Thomas <mikethomas84@gmail.com>
2025-01-22 15:36:37 +0000

    make note factory

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User;
 
 
 class Note extends Model
 {
-    use HasUuids;
+    use HasFactory,HasUuids;
 
     protected $guarded = [
         'id'
