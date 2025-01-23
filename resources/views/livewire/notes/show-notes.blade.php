26d2a15e5d4c615e1a6c15ea120974d5f5d98cc3
Mike Thomas <mikethomas84@gmail.com>
2025-01-23 14:21:30 +0000

    create note buttons

--- resources/views/livewire/notes/show-notes.blade.php
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -12,30 +12,43 @@ new class extends Component {
 }; ?>
 
 <div class="space-y-2">
-    <div class="grid grid-cols-3 gap-4">
-    @foreach( $notes as $note )
-        <x-card wire:key='{{ $note->id }}'>
-            <div class="flex justify-between">
-                <div>
-                    <a href=""
-                        class="text-xl font-bold hover:underline hover:text-blue-500">
-                        {{ $note->title }}
-                    </a>
-                </div>
-                <div class="text-xs text-gray-500">
-                    {{ \Carbon\Carbon::parse($note->send_date)->format('M-d-Y') }}
-                </div>
-            </div>
-            <div class="flex items-end justify-between mt-4 space-x-1">
-                <p class="text-xs">Recipient: <span class="font-semibold">{{ $note->recipient }}</span></p>
-                <div>
-                    <x-mini-button rounded outline icon="eye" href="#">
-                    </x-button.circle>
-                    <x-mini-button rounded outline icon="trash" href="#">
-                    </x-button.circle>
-                </div>
-            </div>
-        </x-card>
-    @endforeach
+    @if ($notes->isEmpty())
+    <div class="text-center">
+        <p class="text-xl font-bold">No notes yet</p>
+        <p class="text-sm">Let's create your first note to send.</p>
+        <x-button primary icon-right="plus" class="mt-6" href="{{ route('notes.create') }}" wire:navigate>
+            Create note
+        </x-button>
     </div>
+    @else
+        <x-button class="mb-12" primary icon-right="plus" href="{{ route('notes.create') }}" wire:navigate>
+            Create note
+        </x-button>
+        <div class="grid grid-cols-3 gap-4">
+        @foreach( $notes as $note )
+            <x-card wire:key='{{ $note->id }}'>
+                <div class="flex justify-between">
+                    <div>
+                        <a href=""
+                            class="text-xl font-bold hover:underline hover:text-blue-500">
+                            {{ $note->title }}
+                        </a>
+                    </div>
+                    <div class="text-xs text-gray-500">
+                        {{ \Carbon\Carbon::parse($note->send_date)->format('M-d-Y') }}
+                    </div>
+                </div>
+                <div class="flex items-end justify-between mt-4 space-x-1">
+                    <p class="text-xs">Recipient: <span class="font-semibold">{{ $note->recipient }}</span></p>
+                    <div>
+                        <x-mini-button rounded outline icon="eye" href="#">
+                        </x-button.circle>
+                        <x-mini-button rounded outline icon="trash" href="#">
+                        </x-button.circle>
+                    </div>
+                </div>
+            </x-card>
+        @endforeach
+        </div>
+    @endif
 </div>
